3244f2a426d4c1f0940422aabc7a8fa223ad66fd
gjs25 <114452703+gjs25@users.noreply.github.com>
2022-10-05 07:15:24 -1000

    python is cool

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,13 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+	for x in range(num,0,-1): #num is the input we give and we want to create a step range that decrements by 1
+		if x == 1:  #change to no more bottles
+			print (f"1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall!")
+		
+		elif x ==2: #change to singular 
+			print (f"2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.")
+			
+		elif x > 1: #exercise for loop 
+			print (f"{x} bottles of beer on the wall, {x} bottles of beer. Take one down and pass it around, {x-1} bottles of beer on the wall.")
 
 
-# bottle_song()
+(bottle_song(50))
